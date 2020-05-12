0761a5dd3b92f4ccdb47918e99c815ae9c372901
burduleaFM <alexandru.toloaca@gmail.com>
2020-05-12 19:11:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,12 +12,6 @@ app.secret_key = os.environ['APP_SECRET_KEY']
 
 @app.route("/", methods=('GET', 'POST'))
 def signup():
-    form = SignupForm()
-    if form.validate_on_submit():
-        signup = Signups(name=form.name.data, email=form.email.data, date_signed_up=datetime.datetime.now())
-        db_session.add(signup)
-        db_session.commit()
-        return redirect(url_for('success'))
     return render_template('signup.html', form=form)
 
 @app.route("/success")
