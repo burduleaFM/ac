d13bb20323cda22a39969600cfd08ee7434c1cf4
burduleaFM <alexandru.toloaca@gmail.com>
2020-05-12 19:32:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from models import Signups
 from database import db_session
 
 app = Flask(__name__)
-app.secret_key = os.environ['APP_SECRET_KEY']
+
 
 @app.route("/", methods=('GET', 'POST'))
 def signup():
